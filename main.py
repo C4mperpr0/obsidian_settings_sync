7e4d0a02513e39fe3d507bd21839435b3be04c2e
C4mperpr0 <Carlbellgardt@gmail.com>
2024-03-21 22:35:01 +0100

    sync

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import json
+import os
 
 OBSIDIAN_CONFIG_PATH = "/home/carl/.config/obsidian/obsidian.json"
 
 class Vault:
-  def __init__(data):
-    self.id = data.keys()[0]
-    self.path = data[0]["path"]
-    self.timestamp = data[0]["ts"]
-    self.is_open = data[0]["open"]
+  def __init__(self, id, data):
+    self.id = id
+    self.name = os.path.basename(os.path.normpath(data["path"]))
+    self.path = data["path"]
+    self.timestamp = data["ts"]
+    self.is_open = data.get("open", False)
+
+  def __repr__(self):
+    return f"{self.id} | \"{self.name}\"{' <open>' if self.is_open else ''}"
 
 def load_config():
   with open(OBSIDIAN_CONFIG_PATH, "r") as file:
     return json.loads(file.read())
 
-conf = load_config()
-print(conf["vaults"])
-vault = Vault(conf["vaults"][0])
-print(vault)
+def get_all_vaults(config=load_config()):
+  vaults = []
+  for v in config["vaults"]:
+    vaults.append(Vault(v, config["vaults"][v]))
+  return vaults
+
+vaults = get_all_vaults()
+for v in vaults:
+  print(v)
